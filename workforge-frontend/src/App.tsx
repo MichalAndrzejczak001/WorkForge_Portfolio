2d4c2db1d71f86c2d00e6dea2a4a4b95c67403a1
Michal Andrzejczak <michandrzejczakofficial@gmail.com>
2026-07-25 00:05:57 +0200

    feat(workforge-frontend): protect home route behind authentication

--- workforge-frontend/src/App.tsx
+++ workforge-frontend/src/App.tsx
@@ -1,12 +1,15 @@
 import { Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage.tsx'
 import LoginPage from './pages/LoginPage.tsx'
+import ProtectedRoute from "./components/ProtectedRoute.tsx";
 
 function App() {
     return (
         <Routes>
-            <Route path="/" element={<HomePage />} />
             <Route path="/login" element={<LoginPage />} />
+            <Route element={<ProtectedRoute />}>
+                <Route path="/" element={<HomePage />} />
+            </Route>
         </Routes>
     )
 }
