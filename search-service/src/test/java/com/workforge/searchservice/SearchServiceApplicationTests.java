362a3c9c21420a69b0d0438856d121c77fbad337
Michal Andrzejczak <michandrzejczakofficial@gmail.com>
2026-07-30 22:26:04 +0200

    fix(search-service): use Testcontainers for Elasticsearch in context test

--- search-service/src/test/java/com/workforge/searchservice/SearchServiceApplicationTests.java
+++ search-service/src/test/java/com/workforge/searchservice/SearchServiceApplicationTests.java
@@ -2,12 +2,28 @@ package com.workforge.searchservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.elasticsearch.ElasticsearchContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
+@Testcontainers
 @SpringBootTest
 class SearchServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Container
+    static ElasticsearchContainer elasticsearch =
+            new ElasticsearchContainer("docker.elastic.co/elasticsearch/elasticsearch:9.0.0")
+                    .withEnv("xpack.security.enabled", "false");
 
+    @DynamicPropertySource
+    static void overrideProps(DynamicPropertyRegistry registry) {
+        registry.add("spring.elasticsearch.uris", () -> "http://" + elasticsearch.getHttpHostAddress());
+    }
+
+    @Test
+    void contextLoads() {
+    }
 }
+
